b45947e79d6170586e170efa8d2e4479110c93c0
Rana Emad <rana3emad@gmail.com>
2020-06-22 16:37:52 +0200

    basic dog layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Dog from "./components/Dog/Dog";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Dog />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Dog/Dog.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Dog.css";
+
+class Dog extends React.Component {
+  constructor() {
+    super();
+    this.url = "https://dog.ceo/api/breeds/image/random";
+    this.state = {
+      img: "",
+    };
+  }
+
+  componentDidMount() {
+    this.fetchImg();
+  }
+
+  fetchImg = () => {
+    fetch(this.url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.status === "success") {
+          this.setState({ img: data.message });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  render() {
+    return (
+      <div className="dog">
+        <img alt="dog" src={this.state.img} />
+        <button className="btn btn-bright">WOOF!</button>
+      </div>
+    );
+  }
+}
+
+export default Dog;
