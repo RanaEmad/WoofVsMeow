f5e9d04c11698e3c9e2e1af906358d930feacc82
Rana Emad <rana3emad@gmail.com>
2020-06-23 01:14:00 +0200

    Updates

---
+++ precache-manifest.eed0112af6e3759ad64a1070552bc414.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168e930f7c5adf56da4eace78501c307",
+    "url": "/woof-vs-meow/index.html"
+  },
+  {
+    "revision": "cf6f0fde083bebd10f99",
+    "url": "/woof-vs-meow/static/css/main.497072e3.chunk.css"
+  },
+  {
+    "revision": "f14c80bcb8a1e1822a1e",
+    "url": "/woof-vs-meow/static/js/2.fb8bea3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/woof-vs-meow/static/js/2.fb8bea3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6f0fde083bebd10f99",
+    "url": "/woof-vs-meow/static/js/main.b815a1e0.chunk.js"
+  },
+  {
+    "revision": "02611d8c0e9cc62894ad",
+    "url": "/woof-vs-meow/static/js/runtime-main.d07845ca.js"
+  },
+  {
+    "revision": "b38a9e9c0f88a95de9c87421e78ffc08",
+    "url": "/woof-vs-meow/static/media/cat.b38a9e9c.svg"
+  },
+  {
+    "revision": "9944c85cbe4b1f6220ec52c897d3236d",
+    "url": "/woof-vs-meow/static/media/dog.9944c85c.svg"
+  },
+  {
+    "revision": "2333bfdad85ba0e8847034f8f680abe9",
+    "url": "/woof-vs-meow/static/media/paws.2333bfda.jpg"
+  }
+]);
